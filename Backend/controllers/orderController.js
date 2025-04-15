1962a157d3b7b6610c1d62faf832baaac3f5f63a
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-04-15 18:27:52 +0530

    k

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,8 +1,8 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Placing User Order For Frontend.
 const placeOrder = async (req,res) => { 
